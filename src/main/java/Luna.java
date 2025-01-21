25252d1c1438a892d37bf18208f362189b7760fa
Yu Sheng <kohyusheng@gmail.com>
2025-01-21 22:15:40 +0800

    Level 1
    Echo commands

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Luna {
+
+    Scanner sc = new Scanner(System.in);
+
     public void greet() {
         System.out.println("Hello! I'm " + this.getClass().getSimpleName() + "\nWhat can I do for you?");
     }
 
     public void exit() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public void echo() {
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                exit();
+                break;
+            } else {
+                System.out.println(command); // Just print the command itself
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -16,8 +33,8 @@ public class Luna {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
          */
-        Luna chatbot = new Luna();
-        chatbot.greet();
-        chatbot.exit();
+        Luna chatBot = new Luna();
+        chatBot.greet();
+        chatBot.echo();
     }
 }
